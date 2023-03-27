3194e3cecfc1810850078478b375006edad129cb
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-03-27 13:59:58 +0300

    Initial Commit

---
+++ MVP Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP Example
+//
+//  Created by Ilya Vasilev on 27.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
