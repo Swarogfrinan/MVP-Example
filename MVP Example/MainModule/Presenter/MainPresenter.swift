12b9fdb71e6c8a237565075a8aec1b116246ea4b
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-03-28 17:37:24 +0300

    Changed Presenter Main and update test

--- MVP Example/MainModule/Presenter/MainPresenter.swift
+++ MVP Example/MainModule/Presenter/MainPresenter.swift
@@ -7,23 +7,31 @@ protocol MainViewProtocol {
 }
 
 protocol MainViewPresenterProtocol {
-    init(view: MainViewProtocol, networkService : NetworkService)
+    init(view: MainViewProtocol, networkService : NetworkServiceProtocol, router: RouterProtocol)
     func getComments()
     var comments : [Comment]? { get set }
-    
+    func tapOnTheComment(comment: Comment?)
 }
 
 class MainPresenter : MainViewPresenterProtocol {
+    func tapOnTheComment(comment: Comment?) {
+        router?.showDetail(comment: comment)
+    }
     
     var comments: [Comment]?
-     var view : MainViewProtocol?
-    let networkService : NetworkService!
+    var view : MainViewProtocol?
+    let networkService : NetworkServiceProtocol!
+    let router : RouterProtocol?
     
-    required init(view: MainViewProtocol, networkService: NetworkService) {
+    required init(view: MainViewProtocol, networkService: NetworkServiceProtocol, router: RouterProtocol) {
         self.view = view
         self.networkService = networkService
+        self.router = router
         getComments()
     }
+    
+   
+    
     
     func getComments() {
         networkService.getComments { [weak self] result in
--- MVP Example/MainModule/Presenter/MainPresenterTest.swift
+++ MVP Example/MainModule/Presenter/MainPresenterTest.swift
@@ -8,60 +8,76 @@ import XCTest
 @testable import MVP_Example
 
 class MockView : MainViewProtocol {
-    var titleTest : String?
-    func setGreeting(greeting: String) {
-        self.titleTest = greeting
+    func succes() {
+        
+    }
+    
+    func failure(error: Error) {
+        
+    }
+}
+
+class MockNetworkService : NetworkServiceProtocol {
+    
+    var comments : [Comment]!
+    
+    init() {}
+    
+    convenience init(comments : [Comment]?) {
+        self.init()
+        self.comments = comments
+    }
+    
+    func getComments(completion: @escaping (Result<[MVP_Example.Comment]?, Error>) -> Void) {
+        if let comments = comments {
+            completion(.success(comments))
+        } else {
+            let error = NSError(domain: "", code: 0, userInfo: nil)
+            completion(.failure(error))
+        }
     }
 }
 
 final class MainPresenterTest: XCTestCase {
     
     var view : MockView!
-    var person : Person!
     var presenter : MainPresenter!
+    var networkService : NetworkServiceProtocol!
+    var comments = [Comment]()
+    var router : RouterProtocol!
     
     override func setUpWithError() throws {
         view = MockView()
-        person = Person(firstName: "Baz", lastName: "CocoJaz")
-        presenter = MainPresenter(view: view, person: person)
+        let navigationController = UINavigationController()
+        let assemblyBuilder = AssemblyBuilder()
+router = Router(navigationController: navigationController, assemblyBuilder: assemblyBuilder)
+        
     }
-
+    
     override func tearDownWithError() throws {
         view = nil
-        person = nil
+        networkService = nil
         presenter = nil
     }
     
-    func testModuleIsNotNil() {
-    XCTAssertNotNil(view, "view is not nill")
-        XCTAssertNotNil(person, "person is not nill")
-        XCTAssertNotNil(presenter, "presenter is not nill")
-    }
-    
-    func testView() {
-        presenter.showGreeting()
-        XCTAssertEqual(view.titleTest, "BazCocoJaz")
-    }
-    
-    func testPersonModel() {
-        XCTAssertEqual(person.firstName, "Baz")
-        XCTAssertEqual(person.lastName, "CocoJaz")
-    }
-    
-    
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGetSucces() {
+        let comment = Comment(postId: 1, id: 2, name: "Bar", email: "Baz", body: "Foo")
+        comments.append(comment)
+        
+        view = MockView()
+        networkService = MockNetworkService(comments: [comment])
+        presenter = MainPresenter(view: view, networkService: networkService, router: router)
+        var catchComments : [Comment]?
+        
+        networkService.getComments { result in
+            switch result {
+            case .success(let comments):
+                catchComments = comments
+            case .failure(let error):
+                print(error)
+            }
         }
+        XCTAssertNotEqual(catchComments?.count,  0)
+        XCTAssertEqual(catchComments?.count,  comments.count)
     }
-
 }
